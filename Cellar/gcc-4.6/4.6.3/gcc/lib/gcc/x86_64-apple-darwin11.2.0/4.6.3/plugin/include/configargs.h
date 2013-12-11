3c5697bb2d35a0770cc1f5d4bd125fb9c69002a9
Mozilla Release Engineering <release@mozilla.com>
2013-12-11 05:40:51 -0800

    Built gcc with c++ support

--- Cellar/gcc-4.6/4.6.3/gcc/lib/gcc/x86_64-apple-darwin11.2.0/4.6.3/plugin/include/configargs.h
+++ Cellar/gcc-4.6/4.6.3/gcc/lib/gcc/x86_64-apple-darwin11.2.0/4.6.3/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../configure --enable-languages=c --prefix=/usr/local/Cellar/gcc-4.6/4.6.3/gcc --datarootdir=/usr/local/Cellar/gcc-4.6/4.6.3/share --bindir=/usr/local/Cellar/gcc-4.6/4.6.3/bin --program-suffix=-4.6 --with-gmp=/usr/local/Cellar/gmp/5.1.3 --with-mpfr=/usr/local/Cellar/mpfr/3.1.2 --with-mpc=/usr/local/Cellar/libmpc/1.0.1 --with-system-zlib --enable-stage1-checking --enable-plugin --enable-lto --disable-multilib --disable-nls";
+static const char configuration_arguments[] = "../configure --enable-languages=c,c++ --prefix=/usr/local/Cellar/gcc-4.6/4.6.3/gcc --datarootdir=/usr/local/Cellar/gcc-4.6/4.6.3/share --bindir=/usr/local/Cellar/gcc-4.6/4.6.3/bin --program-suffix=-4.6 --with-gmp=/usr/local/Cellar/gmp/5.1.3 --with-mpfr=/usr/local/Cellar/mpfr/3.1.2 --with-mpc=/usr/local/Cellar/libmpc/1.0.1 --with-system-zlib --enable-stage1-checking --enable-plugin --enable-lto --disable-multilib --disable-nls";
 static const char thread_model[] = "posix";
 
 static const struct {
