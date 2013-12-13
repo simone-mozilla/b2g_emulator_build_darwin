7ac6a4198e4bf36491b3f5fdc891c235c188d2bb
Mozilla Release Engineering <release@mozilla.com>
2013-12-13 08:22:24 -0800

    removed output from apply_dev_patch.sh

--- apply_dev_patch.sh
+++ apply_dev_patch.sh
@@ -2,8 +2,5 @@
 
 while ! cd "$(dirname "${0}")/build-dir/build/sdk" || ! curl https://github.com/marshall/android-sdk/commit/d52cb5f1a47d98c5d961b9e3885072744522ec43.diff 2>/dev/null | patch -p1 -f >/dev/null 2>&1
 do
-    echo "Patch not applied"
     sleep 3
 done
-
-echo "Patch applied"
