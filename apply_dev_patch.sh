d88cc9c2220361f7160921bf96d3a7affd4c0130
Mozilla Release Engineering <release@mozilla.com>
2013-12-17 05:09:47 -0800

    turned off debug output

--- apply_dev_patch.sh
+++ apply_dev_patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! cd "$(dirname "${0}")/build-dir/build/sdk" || ! curl https://github.com/marshall/android-sdk/commit/d52cb5f1a47d98c5d961b9e3885072744522ec43.diff 2>/dev/null | patch -p1 -f >/dev/null 2>&1
+while ! cd "$(dirname "${0}")/build-dir/build/sdk" 2>/dev/null || ! curl https://github.com/marshall/android-sdk/commit/d52cb5f1a47d98c5d961b9e3885072744522ec43.diff 2>/dev/null | patch -p1 -f >/dev/null 2>&1
 do
     sleep 3
 done
