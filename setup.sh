95a90038af78cf46fb85dd6582d607c90b2063fc
Mozilla Release Engineering <release@mozilla.com>
2014-02-26 06:30:25 -0800

    added stuff

--- setup.sh
+++ setup.sh
@@ -18,19 +18,17 @@ cd "$(dirname "${0}")"
     curl http://artfiles.org/gnupg.org/gnupg/gnupg-1.4.15.tar.bz2 -o /Library/Caches/Homebrew/gnupg-1.4.15.tar.bz2
     
     # ./bootstrap-mac.sh
-    /usr/bin/sudo /bin/chmod g+rwx /usr/local/. /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/lib/pkgconfig /usr/local/Library /usr/local/sbin /usr/local/share /usr/local/var /usr/local/var/log /usr/local/share/man /usr/local/share/man/man1 /usr/local/share/man/man3 /usr/local/share/man/man5 /usr/local/share/man/man7 /usr/local/share/info /usr/local/share/doc
-    /usr/bin/sudo /usr/bin/chgrp admin /usr/local/. /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/lib/pkgconfig /usr/local/Library /usr/local/sbin /usr/local/share /usr/local/var /usr/local/var/log /usr/local/share/man /usr/local/share/man/man1 /usr/local/share/man/man3 /usr/local/share/man/man5 /usr/local/share/man/man7 /usr/local/share/info /usr/local/share/doc
-    mktemp /tmp/b2g-boostrap.XXXXX
-    curl -fsSL https://raw.github.com/mxcl/homebrew/go/install -o /tmp/b2g-bootstrap.XXXXX
-    ruby /tmp/b2g-bootstrap.XXXXX
+    /usr/bin/sudo /bin/chmod g+rwx /usr/local /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/lib/pkgconfig /usr/local/Library /usr/local/sbin /usr/local/share /usr/local/var /usr/local/var/log /usr/local/share/man /usr/local/share/man/man1 /usr/local/share/man/man3 /usr/local/share/man/man5 /usr/local/share/man/man7 /usr/local/share/info /usr/local/share/doc
+    /usr/bin/sudo /usr/bin/chgrp admin /usr/local /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/lib/pkgconfig /usr/local/Library /usr/local/sbin /usr/local/share /usr/local/var /usr/local/var/log /usr/local/share/man /usr/local/share/man/man1 /usr/local/share/man/man3 /usr/local/share/man/man5 /usr/local/share/man/man7 /usr/local/share/info /usr/local/share/doc
+
+    B2G_BOOTSTRAP="$(mktemp /tmp/b2g-boostrap.XXXXX)"
+    curl -fsSL https://raw.github.com/mxcl/homebrew/go/install -o "${B2G_BOOTSTRAP}"
+    ruby "${B2G_BOOTSTRAP}"
     sudo mkdir -p /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
     sudo ln -s /Developer/SDKs/MacOSX10.6.sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/
     /usr/local/bin/brew install gpg
     /usr/local/bin/brew install cmake
     /usr/local/bin/brew install https://raw.github.com/mozilla-b2g/B2G/master/scripts/homebrew/gcc-4.6.rb
-
-
-
     
     ln -s gcc-4.6 /usr/local/bin/gcc
     
